854dc7313195d9554f70576495d5a56085177d92
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2021-03-09 23:07:19 +0000

    Install material-ui

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
     <div>
       <h1>This is the header</h1>
+
+      <div className="header__left">
+        <img src="" alt="" />
+        <div className="header__search">
+          {/* Search icon */}
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
     </div>
   );
 }
