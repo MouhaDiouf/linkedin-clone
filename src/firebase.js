a505a474ecbd2489564e375c935bc23e873911fd
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2021-03-09 22:29:12 +0000

    Initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIA-1bO4jyHSOq_Gs5S06LaE1U3bv_5So",
+  authDomain: "linkedin-clone-b3c77.firebaseapp.com",
+  projectId: "linkedin-clone-b3c77",
+  storageBucket: "linkedin-clone-b3c77.appspot.com",
+  messagingSenderId: "278929542713",
+  appId: "1:278929542713:web:8c80a6089ffd079acd07e8",
+};
